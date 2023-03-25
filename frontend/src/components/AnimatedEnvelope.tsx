4127a8920dd2e8ffee655bb9d061e6221a8ce457
AG Samaniego <asama6@illinois.edu>
2023-03-25 14:34:57 -0500

    Envelope WIP

---
+++ frontend/src/components/AnimatedEnvelope.tsx
@@ -0,0 +1,50 @@
+
+function AnimatedEnvelope() {
+
+  return (
+    <div>
+    <div className="m-0 p-0 sm:box-border">
+    </div>
+    <div className="wrapper h-200px w-300px bg-gray-500 relative flex justify-center items-center z-0"> 
+    <div className="absolute top-0 w-4/5 h-4/5 bg-white rounded-15 z-2"> 
+    </div>
+    </div>
+    
+    
+    </div>
+
+    /* .wrapper {
+        height: 200px;
+        width: 300px;
+        background-color: #3B4049;
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align items: center;
+        z-index: 0;
+    }
+    .letter {
+        position: absolute;
+        top: 0;
+        width: 80%;
+        height: 80%;
+        background-color: white;
+        border-radius: 15px;
+        z-index: 2;
+    }
+    .envelope {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        left: 0;
+        border-top: 100px solid black;
+        border-right:150px solid black;
+        border-bottom: 100px solid black;
+        border-left: 150px solid black;
+        z-index: 3;
+    }*/
+  )
+}
+
+export default AnimatedEnvelope

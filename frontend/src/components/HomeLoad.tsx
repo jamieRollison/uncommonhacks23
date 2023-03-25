caeb9369c5a17378f9139a4e12c3e2ceda2364dd
estrella <epopo2@illinois.edu>
2023-03-25 13:40:30 -0500

    Add fonts, styles, and update tailwind config

---
+++ frontend/src/components/HomeLoad.tsx
@@ -0,0 +1,24 @@
+function HomeLoad() {
+
+  return (
+  <div className="">
+    <h1 className="mb-2 font-BuonaDisplay text-4xl text-gray-100 md:text-6xl">
+        
+        <span className="relative">
+    <span className="h-20 pt-2 overflow-x-hidden whitespace-nowrap ">
+      Letra <span className="text-3xl md:text-5xl"></span>
+    </span>
+    <span
+      className=" cursor absolute -bottom-0 left-0 -top-1 inline-block bg-berryblue w-full animate-type will-change"
+    ></span>
+  </span>
+</h1>
+<p className="text-white font-serif">
+    Welcome to what we like to call, a working progress of the internet. <span className="italic">Letra</span> is a place where you can share your thoughts, ideas, and opinions with the world.
+</p>
+</div>
+
+  )
+}
+
+export default HomeLoad
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -3,7 +3,27 @@ module.exports = {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        berryblue: "#3b3d5b",
+      },
+      animation: {
+        type: 'type 2.7s ease-out .8s infinite alternate both',
+      },
+      keyframes: {
+        type: {
+          '0%': { transform: 'translateX(0ch)' },
+          '5%, 10%': { transform: 'translateX(1ch)' },
+          '15%, 20%': { transform: 'translateX(2ch)' },
+          '25%, 30%': { transform: 'translateX(3ch)' },
+          '35%, 40%': { transform: 'translateX(4ch)' },
+
+        },
+      },
+      fontFamily: {
+        BuonaDisplay: ["BuonaDisplay", "serif"],
+      },
+    },
   },
   plugins: [],
 }

c3cb126f8b4cc4975cd7d201ea2cd3b3c515617b
jamieRollison <rollisonjamie15@gmail.com>
2023-03-26 02:03:00 -0500

    update frontend api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -14,3 +14,17 @@ export const createNote = async (note: Note) => {
   const { data } = await api.post('/notes', note);
   return data;
 }
+
+export const getNote = async (id: string) => {
+  const { data } = await api.get(`/notes/${id}`);
+  const content = await getContent(data.content);
+  return {
+    ...data,
+    content,
+  };
+}
+
+export const getContent = async (id: string) => {
+  const { data } = await api.get(`/content/${id}`);
+  return data;
+}
